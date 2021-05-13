c9d277a9af3abb30e8bddda2ebd93b9b62f634e2
Etienne Thompson <et@etiennethompson.com>
2021-05-12 20:28:52 -0700

    Converted to yarn. Fixed up about me section.

--- src/App.tsx
+++ src/App.tsx
@@ -18,24 +18,24 @@ function App() {
       </div>
       <div className="aboutMe">
         <div className="header">
-          <p>About Me</p>
+          <p className="header-text">About Me</p>
         </div>
-        <div className="text">
-          <p>
+        <div className="description">
+          <p className="description-text">
             I'm a current Junior studying Computer Science and Chinese at the
             University of Arizona. I'm interested currently in web development
             and learning the full stack. I've worked mostly on some front end
             projects, and want to delve deeper into back end frameworks and
             design, and looking into different hosting service options like
-            serverless.
-          </p>
-          <p>
-            In my free time I like to play trading card games. Right now I
-            mainly play the Commander format of Magic: the Gathering, but I've
-            played a few others as well. I started playing the Pokemon Trading
-            Card Game back in 2008 with my dad. I also like to draw! I'm a self
-            taught artist who likes colored pencil and normal pencil medium
-            drawings. You can check out my artwork below.
+            serverless. You can check out a few of the projects I've worked on
+            down below.
+            <br />
+            <br /> In my free time I like to play trading card games. Right now
+            I mainly play the Commander format of Magic: the Gathering, but
+            I've played a few others as well. I started playing the Pokemon
+            Trading Card Game back in 2008 with my dad. I also like to draw!
+            I'm a self taught artist who likes colored pencil and normal pencil
+            medium drawings. You can check out my artwork below.
           </p>
         </div>
       </div>
