40b63149650ea7b44f55c7bf4f72138231798a1e
Etienne Thompson <43585499+EtienneThompson@users.noreply.github.com>
2021-06-30 19:53:53 -0700

    Make art images single column on mobile. (#7)
    * Update art rows and columns to components.
    
    * Split art into mobile and desktop components.
    
    * Increase row padding.

---
+++ src/components/Art/Art.mobile.tsx
@@ -0,0 +1,38 @@
+import { Row } from "../common/Grid";
+import "./Art.scss";
+
+import colored_tiger from "../../assets/images/colored_tiger.jpg";
+import pencil_owl from "../../assets/images/pencil_owl.jpg";
+import colored_lion_1 from "../../assets/images/colored_lion_1.jpg";
+import colored_horse from "../../assets/images/colored_horse.jpg";
+
+export const MobileArt = () => {
+  return (
+    <div>
+      <Row justify="center">
+        <img
+          className="image"
+          src={colored_tiger}
+          alt="Colored Pencil Tiger"
+        />
+      </Row>
+      <Row justify="center">
+        <img className="image" src={pencil_owl} alt="Pencil Owl" />
+      </Row>
+      <Row justify="center">
+        <img
+          className="image"
+          src={colored_lion_1}
+          alt="Colored Pencil Lion"
+        />
+      </Row>
+      <Row justify="center">
+        <img
+          className="image"
+          src={colored_horse}
+          alt="Colored Pencil Horse"
+        />
+      </Row>
+    </div>
+  );
+};
--- src/components/Art/Art.tsx
+++ src/components/Art/Art.tsx
@@ -1,45 +1,15 @@
 import { SectionHeader } from "../common/SectionHeader";
 import { FadeInSection } from "../common/FadeInSection";
+import { MobileArt, DesktopArt } from ".";
 import "./Art.scss";
-
-import colored_tiger from "../../assets/images/colored_tiger.jpg";
-import pencil_owl from "../../assets/images/pencil_owl.jpg";
-import colored_lion_1 from "../../assets/images/colored_lion_1.jpg";
-import colored_horse from "../../assets/images/colored_horse.jpg";
+import { isMobile } from "../../utils";
 
 export const Art = () => {
   return (
     <div className="art">
       <FadeInSection>
         <SectionHeader>Art</SectionHeader>
-        <div className="row">
-          <div className="column">
-            <img
-              className="image"
-              src={colored_tiger}
-              alt="Colored Pencil Tiger"
-            />
-          </div>
-          <div className="column">
-            <img className="image" src={pencil_owl} alt="Pencil Owl" />
-          </div>
-        </div>
-        <div className="row">
-          <div className="column">
-            <img
-              className="image"
-              src={colored_lion_1}
-              alt="Colored Pencil Lion"
-            />
-          </div>
-          <div className="column">
-            <img
-              className="image"
-              src={colored_horse}
-              alt="Colored Pencil Horse"
-            />
-          </div>
-        </div>
+        {isMobile ? <MobileArt /> : <DesktopArt />}
       </FadeInSection>
     </div>
   );
