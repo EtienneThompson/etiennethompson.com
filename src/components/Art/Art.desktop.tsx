fba225791a571f6ef2cd6532e678a5c2a1dc0375
Etienne Thompson <et@etiennethompson.com>
2021-08-20 15:41:23 -0700

    Add new lion image to art section.

--- src/components/Art/Art.desktop.tsx
+++ src/components/Art/Art.desktop.tsx
@@ -4,11 +4,22 @@ import "./Art.scss";
 import colored_tiger from "../../assets/images/colored_tiger.jpg";
 import pencil_owl from "../../assets/images/pencil_owl.jpg";
 import colored_lion_1 from "../../assets/images/colored_lion_1.jpg";
+import colored_lion_2 from "../../assets/images/colored_lion_2.jpeg";
 import colored_horse from "../../assets/images/colored_horse.jpg";
 
 export const DesktopArt = () => {
   return (
     <div>
+      <Row justify="center">
+        <Col cols="2">
+          <img
+            className="desktop image"
+            src={colored_lion_2}
+            alt=""
+            loading="lazy"
+          />
+        </Col>
+      </Row>
       <Row justify="center">
         <Col cols="2">
           <img
--- src/components/Art/Art.mobile.tsx
+++ src/components/Art/Art.mobile.tsx
@@ -4,11 +4,15 @@ import "./Art.scss";
 import colored_tiger from "../../assets/images/colored_tiger.jpg";
 import pencil_owl from "../../assets/images/pencil_owl.jpg";
 import colored_lion_1 from "../../assets/images/colored_lion_1.jpg";
+import colored_lion_2 from "../../assets/images/colored_lion_2.jpeg";
 import colored_horse from "../../assets/images/colored_horse.jpg";
 
 export const MobileArt = () => {
   return (
     <div>
+      <Row justify="center">
+        <img className="image" src={colored_lion_2} alt="" loading="lazy" />
+      </Row>
       <Row justify="center">
         <img
           className="image"
